62c4044a2ccda3d97a474d9ee3367a4e0b9941f0
Casey <caseyderiso@gmail.com>
2020-10-05 19:02:06 -0500

    created prompt for robot name and concatenated string and variable to display robot name in a console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("This is an altert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName + " is ready for battle!");
+
+// create fight function 
+
+function fight() {
+    window.alert("The Fight Has Begun!")
+}
+
+// fight();
